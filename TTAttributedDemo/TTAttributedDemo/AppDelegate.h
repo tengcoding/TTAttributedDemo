138cd786502453a6605210dc78000d90cbe3ecd8
TT_dashen <ltt143@126.com>
2016-09-05 15:06:34 +0800

    test

---
+++ TTAttributedDemo/TTAttributedDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TTAttributedDemo
+//
+//  Created by 梁腾 on 16/9/5.
+//  Copyright © 2016年 TT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
